ec6eb2d27bdfe9e6a7d68b8556cb8f15f4b78fdf
EetuPeltolaCodes <eddy.peltola@gmail.com>
2024-12-18 14:59:04 +0200

    Update time-based simulation

--- Assignment_time_simulation.m
+++ Assignment_time_simulation.m
@@ -1,7 +1,12 @@
-%% Task 1
+%% Practical Assignment
+% Probabilistic Simulations
+% Authors: Eetu Knutars, Eetu Peltola
+
 clearvars
 close all
 clc
+
+% 
 
 % Initializing the simulation values
 Nsim = 10^4;
@@ -12,8 +17,16 @@ c1_ub = [72, 120];  % 1. constractor's upper boundaries
 c2_lb = [60, 80];   % 2. constractor's lower boundaries
 c2_ub = [80, 130];  % 2. constractor's upper boundaries
 
+splits = (0.4:0.01:0.6);
+plot_means = zeros(1, length(splits));
+n = 1;
 % Simulate with splits 40%/60% - 60%/40% with 1% step
-for i = 0.4:0.01:0.6
+
+% Best split is 52%/48%
+best_split_days = zeros(1, Nsim);
+best_split = 0.52;
+n1 = 1;
+for i = splits
     days = zeros(1, Nsim);
     % Split the batches to the constractors
     c1_shop_need = shop_need * i;
@@ -95,51 +108,39 @@ for i = 0.4:0.01:0.6
         end
         % Calculate the days taken 
         days(j) = (time_taken-time_step) /24;   % (last time step is extra)
+
+        if i == best_split
+            best_split_days(n1) = days(j);
+            n1 = n1+1;
+        end
+
     end
-    disp(['Simulating with split ' , num2str(c1_shop_need) , ' for first contractor ' , num2str(c2_shop_need) , ' for second contractor '])
-    mean_days = mean(days)
-    days_95 = quantile(days, 0.95)
+    mean_days = round(mean(days),2);
+    days_95 = round(quantile(days, 0.95),2);
+    if i == best_split
+        fprintf('<strong>Split %d/%d: average = %.2f, 95%% confidence = %.2f</strong>\n', i*100, (1-i)*100, mean_days, days_95)
+    else   
+        disp(['Split ' , num2str(i*100) , '/' , num2str((1-i)*100) , ': average = ', num2str(mean_days), ', 95% confidence = ', num2str(days_95)])
+    end
+    plot_means(n) = mean_days;
+    n = n+1;
 end
 
-%% 
-clearvars
-close all
-clc
 
+% Make some plots
+temp = shop_need*splits/batch_size;
+plot(temp, plot_means, '-b')
+hold on
+a = plot(temp(min(plot_means)==plot_means), min(plot_means), 'r.', 'MarkerSize',20);
+plot(temp(min(plot_means)==plot_means), min(plot_means), 'ko-', 'MarkerSize',10, 'LineWidth',1)
+xticks(shop_need*splits/batch_size)
+xlabel('Number of Batches Assigned to Contractor 1')
+ylabel('Average Processing Time (Days)')
+title('Impact of Batch Distribution on Processing Time')
+legend(a, 'The Best Processing Time')
+hold off
 
-% Initialize
-Nsim = 10^5;
-lb = [48, 72];
-ub = [72, 120];
-batch_size = 100;
-device_need = 1000;
-days = zeros(1,Nsim);
-
-for i = 1:Nsim
-    % Initialize the simulation
-    spoint_1 = randi([lb(1), ub(1)]);   % Service line 1 time
-    spoint_2 = 0;   % Service line 2 time
-    batches_ready = 0;
-    time_taken = 0;
-    states = [1,0];
-    while batches_ready < device_need   % Simulate until the device need is fullfilled
-        time_taken = time_taken + 1;
-        spoint_1 = max(0, spoint_1 - 1); % Decrement Service Point 1 time
-        if states(2) == 1   % When the line 2 does something
-            spoint_2 = max(0, spoint_2 - 1); % Decrement Service Point 2 time
-            if spoint_2 == 0
-                batches_ready = batches_ready + batch_size; % Add batch to ready count when Service Point 2 is done
-                states(2) = 0;   % Change the state
-            end
-        end
-        if spoint_1 == 0 && spoint_2 == 0 % If Service Point 1 is ready
-            spoint_1 = randi([lb(1), ub(1)]); % Sample Service Point 1 time
-            spoint_2 = randi([lb(2), ub(2)]); % Sample Service Point 2 time
-            states(2) = 1;  % Change the state
-        end
-    end
-    days(i) = (time_taken-1) / 24;  % Calculate days it took to fullfill the order (last hour is extra)
-end
-
-mean_days = mean(days)  % Calculate the mean
-days_95 = quantile(days, 0.95)   % Calculate the 95%-quantile
+histogram(best_split_days)
+xlabel('Processing Time (Days)')
+ylabel('Frequency')
+title('Distribution of Processing Times for the Best Batch Split (52% / 48%)')
